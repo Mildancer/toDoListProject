568d596824c269eba04b032f51dcbd72ed145037
Mildancer <milosznawrat@gmail.com>
2020-07-28 22:50:43 +0200

    first commit v1

---
+++ .idea/main/org.example.ownToDoList/Register.java
@@ -0,0 +1,30 @@
+import org.example.ownToDoList.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "RegistrationServlet", value = "/register")
+public class Register extends HttpServlet {
+
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String name = req.getParameter("name");
+        String lastname = req.getParameter("lastname");
+        User user = new User(name, lastname);
+        users.add(user);
+        req.setAttribute("users", users);
+        RequestDispatcher dispatcher =         //odpowiada za wysylanie requestow dalej
+                getServletContext().getRequestDispatcher("/list.jsp");
+
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2>TODO list</h2>
+
+<form action="register" method="POST">
+    First name: <input type="text" name="name" /> <br/>
+    Last name: <input type="text" name="lastname"/><br/>
+    <input type="submit" value="Sudmit">
+</form>
+
+</body>
+</html>
