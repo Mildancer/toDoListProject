19e1de9ea1ebe5d156e655dafc406e9517939c23
Mildancer <milosznawrat@gmail.com>
2020-07-30 18:28:48 +0200

    first commit v4

--- .idea/main/org.example.list/ToDoList.java
+++ .idea/main/org.example.list/ToDoList.java
@@ -20,23 +20,25 @@ public class ToDoList extends HttpServlet {
 
     boolean isItDone;
 
+
+
         private List<User> toDoList = new ArrayList<>();
 
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
 
-            String name = req.getParameter("name");
-            String lastname = req.getParameter("lastname");
-
-
-            User user = new User(name, lastname);
-            users.add(user);
-            req.setAttribute("users", users);
-            RequestDispatcher dispatcher =         //odpowiada za wysylanie requestow dalej
-                    getServletContext().getRequestDispatcher("/list.jsp");
-
-            dispatcher.forward(req, resp);
+//            String name = req.getParameter("name");
+//            String lastname = req.getParameter("lastname");
+//
+//
+//            User user = new User(name, lastname);
+//            users.add(user);
+//            req.setAttribute("users", users);
+//            RequestDispatcher dispatcher =         //odpowiada za wysylanie requestow dalej
+//                    getServletContext().getRequestDispatcher("/list.jsp");
+//
+//            dispatcher.forward(req, resp);
         }
 
 
